313e5e4d576ed681b91cf78fb5598ce60ad3dd86
unknown <chromegold70@gmail.com>
2022-01-20 12:23:46 -0300

    Add react-virtualized

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import { List, ListRowRenderer } from 'react-virtualized'
 import { ProductItem } from './ProductItem'
 
 interface SearchResultsProps {
@@ -17,11 +18,32 @@ export function SearchResults({
   results,
   onAddToWishList
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    // sempre colocar uma div em volta
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5} //quantos itens para aplicação deixar carregado, quanto para cima e para baixo
+        rowCount={results.length} // quantos itens tem na lista
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map(product => {
         return (
           <ProductItem
             key={product.id}
@@ -29,7 +51,7 @@ export function SearchResults({
             onAddToWishList={onAddToWishList}
           />
         )
-      })}
+      })} */}
     </div>
   )
 }
